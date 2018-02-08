70ed9478dd1c3d349fef52540e95c03387b9025d
kosc <hotkosc@gmail.com>
2018-02-08 10:16:06 +0300

    Update views.py
    PEP-0008 requires two blank lines before function if it out of class.

--- blog/views.py
+++ blog/views.py
@@ -8,9 +8,11 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
 
 def post_new(request):
     if request.method == 'POST':
@@ -25,6 +27,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
@@ -38,4 +41,3 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
-
